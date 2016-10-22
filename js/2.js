466778ddb051f62657c9112578e184065801b155
thu <mingfei.net@gmail.com>
2016-10-22 11:12:23 +0800

    Update

---
+++ js/2.js
@@ -0,0 +1,15 @@
+// 弱变量类型语言
+var test;
+function myFunction() {
+    //var x; // variable int i; boolean b;
+    var x = null;
+    var z;
+    document.getElementById('demo').innerHTML = "<b style='color:red;'>My First JavaScript Function</b>";
+    alert(x);
+    //return 'hi';
+    test = 'abc...';
+}
+
+function test() {
+    test = 'test...';
+}
